b12bf4dc8c51f006c25f0b822fd41d1430be8567
Matheus Gabriel Onofre Alves <davisilbarros@gmail.com>
2023-08-29 12:43:23 -0300

    first commit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,9 @@
+import MainContainer from '@/components/navbar/MainContainer'
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <MainContainer>
+    <Component {...pageProps} />
+  </MainContainer>
 }
---
+++ src/pages/rua.tsx
@@ -0,0 +1,10 @@
+export default function rua(){
+    return (
+        <>
+        <div>
+            <h1>Rua</h1>
+        </div>
+        </>
+        
+    )
+}
